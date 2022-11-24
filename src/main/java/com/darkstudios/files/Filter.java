3e7d70acc7cca4c9eaf18e324f58c32a618e2b37
azrgod <azraelsuperfire@gmail.com>
2022-11-24 02:12:46 -0600

    Initial commit

---
+++ src/main/java/com/darkstudios/files/Filter.java
@@ -0,0 +1,27 @@
+package com.darkstudios.files;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+@Provider
+public class Filter implements ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) {
+//        requestContext.getHeaders().forEach((k, v) -> System.out.println(k + ": " + v.get(0)));
+//        InputStream is = requestContext.getEntityStream();
+//        try {
+//            byte[] data = is.readAllBytes();
+//            String body = new String(data, StandardCharsets.UTF_8);
+//            System.out.println(body);
+//            requestContext.setEntityStream(new ByteArrayInputStream(data));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
